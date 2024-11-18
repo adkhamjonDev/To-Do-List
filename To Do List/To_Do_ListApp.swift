3233969fe13b81a2edcbe93cb4d5cc5659931294
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-18 16:56:23 +0500

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Adkhamjon Rakhimov on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
